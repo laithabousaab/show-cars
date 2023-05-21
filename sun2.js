deb4dca889afed5c61ec72f2d81bf152105ce3e7
laithabousaab <132582804+laithabousaab@users.noreply.github.com>
2023-05-21 17:15:54 +0300

    Add files via upload

---
+++ sun2.js
@@ -0,0 +1,123 @@
+f = document.getElementById('f');
+naf=document.getElementById("naf");
+moon2=document.getElementById("moon2");
+n = document.getElementById('n');
+naf.style.display="none";
+rr = document.getElementById('rr');
+img2 = document.getElementById('img2');
+
+
+
+function test()
+{
+
+   
+    f.style.width="660px";
+   
+}
+    
+
+
+
+
+
+
+
+f.onclick=function()
+
+{
+
+
+    f.style.behavior="smooth";
+    naf.style.display="block";
+    rr.style.display="none";
+   
+
+
+
+}
+
+
+
+
+
+
+
+
+ function hi()
+ 
+ {
+    onpointermove
+    ({
+   
+        left:0,
+        top:0,
+        behavior:"smooth",
+
+
+
+    })
+    
+
+ }
+
+
+
+
+
+
+
+
+
+    
+    window.setTimeout( function() {
+    
+
+    console.log("hi");
+    
+},5000);
+
+
+
+
+
+let w = setInterval(function(){
+
+{
+    
+    n.style.background="#39064db9";
+    rr.style.background="#9905fc";
+    
+
+    
+    n.style.transition= 0.5;
+    
+}
+
+},1000)
+
+
+let e = setInterval(function(){
+    
+    {
+       
+        n.style.background="#012847d2";
+        rr.style.background="#05fcc6";
+    }
+    
+    },2000)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ vewcar.js
@@ -0,0 +1,301 @@
+let aa=document.getElementById("aa");
+let aaa=document.getElementById("aaa");
+let bb=document.getElementById("bb");
+let bbb=document.getElementById("bbb");
+let a=document.getElementById("a");
+let b=document.getElementById("b");
+let c=document.getElementById("c");
+let d=document.getElementById("d");
+let e=document.getElementById("e");
+let f=document.getElementById("f");
+let img3=document.getElementById("img3");
+let img4=document.getElementById("img4");
+let img5=document.getElementById("img5");
+let img6=document.getElementById("img6");
+let img7=document.getElementById("img7");
+let img8=document.getElementById("img8");
+let img9=document.getElementById("img9");
+let merceds=document.querySelector(".merceds");
+let kia=document.querySelector(".kia");
+ let cont =document.getElementById("cont");
+
+
+
+
+let title10=document.getElementById("title10");
+let title20=document.getElementById("title20");
+let title30=document.getElementById("title30");
+let title40=document.getElementById("title40");
+let title50=document.getElementById("title50");
+let title60=document.getElementById("title60");
+let title70=document.getElementById("title70");
+console.log(title40);
+
+
+title10.style.display="none";
+title20.style.display="none";
+title30.style.display="none";
+title40.style.display="none";
+title50.style.display="none";
+title60.style.display="none";
+title70.style.display="none";
+
+
+
+
+
+
+
+
+
+
+bb.style.display="none";
+aa.style.display="none";
+
+img4.style.display="none";
+img5.style.display="none";
+img6.style.display="none";
+img7.style.display="none";
+img8.style.display="none";
+img9.style.display="none";
+
+
+
+
+
+
+
+
+
+
+
+
+
+aaa.onclick=function()
+{
+
+
+    
+    aa.style.display="block";
+
+}
+
+
+a.onclick=function()
+{
+
+
+    title10.style.display="block";
+    img5.style.display="block";
+img4.style.display="none";
+img6.style.display="none";
+img7.style.display="none";
+img8.style.display="none";
+img9.style.display="none";
+
+
+
+title20.style.display="none";
+title30.style.display="none";
+title40.style.display="none";
+title50.style.display="none";
+title60.style.display="none";
+title70.style.display="none";
+
+
+
+}
+
+
+
+b.onclick=function()
+{
+
+
+    title20.style.display="block";
+       img4.style.display="block";
+    img5.style.display="none";
+    img6.style.display="none";
+    img7.style.display="none";
+    img8.style.display="none";
+    img9.style.display="none";
+
+
+
+    title10.style.display="none";
+title30.style.display="none";
+title40.style.display="none";
+title50.style.display="none";
+title60.style.display="none";
+title70.style.display="none";
+    
+
+
+}
+
+
+
+
+
+
+c.onclick=function()
+{
+
+
+    title30.style.display="block";
+       img6.style.display="block";
+    img5.style.display="none";
+    img4.style.display="none";
+    img7.style.display="none";
+    img8.style.display="none";
+    img9.style.display="none";
+
+
+
+    title10.style.display="none";
+    title20.style.display="none";
+    title40.style.display="none";
+    title50.style.display="none";
+    title60.style.display="none";
+    title70.style.display="none";
+    
+
+
+}
+
+
+d.onclick=function()
+{
+
+
+    title40.style.display="block";
+    img7.style.display="block";
+    img5.style.display="none";
+    img4.style.display="none";
+    img6.style.display="none";
+    img8.style.display="none";
+    img9.style.display="none";
+
+
+
+    title30.style.display="none";
+    title10.style.display="none";
+    title20.style.display="none";
+    title50.style.display="none";
+    title60.style.display="none";
+    
+    
+
+
+}
+
+
+e.onclick=function()
+{
+
+
+    title50.style.display="block";
+       img8.style.display="block";
+    img5.style.display="none";
+    img4.style.display="none";
+    img6.style.display="none";
+    img7.style.display="none";
+    img9.style.display="none";
+
+    title40.style.display="none";
+    title10.style.display="none";
+    title20.style.display="none";
+    title60.style.display="none";
+    title30.style.display="none";
+    title70.style.display="none";
+    
+    
+
+
+}
+
+
+
+f.onclick=function()
+{
+
+
+    title60.style.display="block";
+       img9.style.display="block";
+    img5.style.display="none";
+    img4.style.display="none";
+    img6.style.display="none";
+    img7.style.display="none";
+    img8.style.display="none";
+    
+
+    title40.style.display="none";
+    title10.style.display="none";
+    title20.style.display="none";
+    title50.style.display="none";
+    title30.style.display="none";
+    title60.style.display="none";
+
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bbb.onclick=function()
+{
+
+
+    bb.style.display="block";
+
+}
+
+
+
+
+
+
+
+
+let g=setInterval(function(){
+
+
+
+{
+
+    cont.style.background="#03316e";
+}
+
+
+},1000)
+    
+
+
+
+
+
+
+let h=setInterval(function(){
+
+
+
+    {
+    
+        cont.style.background="#2e0247da";
+    }
+    
+    
+    },2000)
